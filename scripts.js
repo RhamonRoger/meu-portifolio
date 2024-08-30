c4af02acb7f301958b8370e9a255a03019c444b3
Rhamon Teixeira <00rrtx@gmail.com>
2024-08-30 15:22:36 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var welcomeButton = document.getElementById('likebutton');
+    
+    welcomeButton.addEventListener('click', function() {
+        alert('Obrigado por visitar o meu site!');
+    });
+});
